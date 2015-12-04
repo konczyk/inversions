919342f71e262a9fb9c3dbbbf1aa384926b5e8a8
Daniel Kończyk <daniel@konczyk.net>
2015-12-04 20:11:55 +0100

    Implement inversions counter

--- inversions.py
+++ inversions.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 class Inversions:
     def __init__(self, items):
         self.items = items;
@@ -11,7 +13,35 @@ class Inversions:
         return self.inversions
 
     def __mergesort(self, items):
-        return items
+        if len(items) <= 1:
+            return items
+
+        mid   = len(items) // 2
+        left  = self.__mergesort(items[:mid])
+        right = self.__mergesort(items[mid:])
+
+        return self.__merge(left, right)
 
     def __merge(self, left, right):
-        return []
+
+        ret = []
+        i, j = 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                ret.append(left[i])
+                i += 1
+            else:
+                ret.append(right[j])
+                # bump inversion count by the amount of items
+                # left in the left list
+                self.inversions += len(left[i:])
+                j += 1
+
+        if i < len(left):
+            ret.extend(left[i:])
+
+        if j < len(right):
+            ret.extend(right[j:])
+
+        return ret
