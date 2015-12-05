4b1f71aa689c460c57a0f5eb4f7bf66242b4d865
Daniel Kończyk <daniel@konczyk.net>
2015-12-05 18:41:23 +0100

    Optimize performance using in-place list ops

--- inversions.py
+++ inversions.py
@@ -3,54 +3,70 @@ import sys
 
 class Inversions:
     def __init__(self, items):
-        self.items = items;
+        self.items = items[:]
         self.inversions = 0
 
     # return the number of inversions
     # in the input array
     def count(self):
-        self.__mergesort(self.items)
+        left, right = 0, len(self.items) - 1
+        self.__mergesort(left, right)
 
         return self.inversions
 
-    def __mergesort(self, items):
-        if len(items) <= 1:
-            return items
+    def __mergesort(self, left, right):
+        # base case
+        if right <= left:
+            return
 
-        mid   = len(items) // 2
-        left  = self.__mergesort(items[:mid])
-        right = self.__mergesort(items[mid:])
+        mid = (left + right) // 2
 
-        return self.__merge(left, right)
+        # recursive calls
+        self.__mergesort(left, mid)
+        self.__mergesort(mid + 1, right)
 
-    def __merge(self, left, right):
+        # merge sublists
+        self.__merge(left, mid, right)
 
-        ret = []
-        i, j = 0, 0
-        ilen, jlen = len(left), len(right)
+    def __merge(self, left, mid, right):
 
-        while i < ilen and j < jlen:
-            if left[i] <= right[j]:
-                ret.append(left[i])
+        # temp storage for sorted sublist
+        tmp = []
+
+        i, j = left, mid + 1
+
+        # classic sort traversal
+        while i <= mid and j <= right:
+            if self.items[i] <= self.items[j]:
+                tmp.append(self.items[i])
                 i += 1
             else:
-                ret.append(right[j])
+                tmp.append(self.items[j])
                 # bump inversion count by the amount of items
-                # left in the left list
-                self.inversions += len(left[i:])
+                # left in the left sublist
+                self.inversions += (mid - i + 1)
                 j += 1
 
-        if i < ilen:
-            ret.extend(left[i:])
+        # shift left leftovers to the right,
+        # if right sublist has been exausted
+        if j > right:
+            last_item = mid
+            move_to = right
+            while last_item >= i:
+                self.items[move_to] = self.items[last_item]
+                move_to -= 1
+                last_item -= 1
 
-        if j < jlen:
-            ret.extend(right[j:])
-
-        return ret
+        # update items with sorted values
+        idx = left
+        for item in tmp:
+            self.items[idx] = item
+            idx += 1
 
 if __name__ == '__main__':
     items = []
     for num in sys.stdin:
         items.append(int(num))
     inv = Inversions(items)
+    del items
     print(inv.count())
