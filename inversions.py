f7b45fad0a1431ce7be085a10521e17bf1b54bed
Daniel Kończyk <daniel@konczyk.net>
2016-01-11 21:15:07 +0100

    Switch to insertion sort for small subarrays

--- inversions.py
+++ inversions.py
@@ -6,15 +6,17 @@ class Inversions:
     Merge sort implementation with inversions counting
     """
 
-    def __init__(self, items):
+    def __init__(self, items, threshold = 10):
         """
         Copy the list of items and set inversions to 0
         Create tmp list of the same size as items to hold merge data
+        Set default threshold to switch to insertion sort
         """
 
         self._items = items[:]
         self._tmp = [0] * len(self._items)
         self._inversions = 0
+        self._threshold = threshold
 
     def count(self):
         """
@@ -32,18 +34,18 @@ class Inversions:
         Sort the array by recursive split using the passed boundaries
         """
 
-        # base case
-        if right <= left:
-            return
+        # switch to insertion sort for small subarrays
+        if right - left <= self._threshold:
+            self._insertion_sort(left, right)
+        else:
+            mid = (left + right) // 2
 
-        mid = (left + right) // 2
+            # recursive calls
+            self._sort(left, mid)
+            self._sort(mid + 1, right)
 
-        # recursive calls
-        self._sort(left, mid)
-        self._sort(mid + 1, right)
-
-        # merge step
-        self._merge(left, mid, right)
+            # merge step
+            self._merge(left, mid, right)
 
     def _merge(self, left, mid, right):
         """
@@ -76,6 +78,21 @@ class Inversions:
                 i += 1
 
             k += 1
+
+    def _insertion_sort(self, left, right):
+        """
+        Sort subarrays indicated by the passed boundaries using
+        insertion sort algorithm
+        """
+        i = left
+        while i <= right:
+            j = i
+            while j > left and self._items[j] < self._items[j-1]:
+                self._items[j-1], self._items[j] = self._items[j], self._items[j-1]
+                # bump inversions counter
+                self._inversions += 1
+                j -= 1
+            i += 1
 
 if __name__ == '__main__':
     items = []
--- test_inversions.py
+++ test_inversions.py
@@ -7,28 +7,64 @@ class InversionsTest(unittest.TestCase):
 
     def test_count_sorted(self):
         items = [1, 2, 3, 4, 5]
-        inv = Inversions(items)
 
+        # merge sort only
+        inv = Inversions(items, 0)
+        self.assertEqual(inv.count(), 0)
+
+        # insertion sort only
+        inv = Inversions(items)
+        self.assertEqual(inv.count(), 0)
+
+        # mixed
+        inv = Inversions(items, 2)
         self.assertEqual(inv.count(), 0)
 
     def test_count_reversed(self):
         items = [5, 4, 3, 2, 1]
-        inv = Inversions(items)
 
+        # merge sort only
+        inv = Inversions(items, 0)
+        self.assertEqual(inv.count(), 10)
+
+        # insertion sort only
+        inv = Inversions(items)
+        self.assertEqual(inv.count(), 10)
+
+        # mixed
+        inv = Inversions(items, 2)
         self.assertEqual(inv.count(), 10)
 
     def test_count_side_inversions(self):
         items = [1, 2, 4, 3]
-        inv = Inversions(items)
-
         # (4,3)
+
+        # merge sort only
+        inv = Inversions(items, 0)
+        self.assertEqual(inv.count(), 1)
+
+        # insertion sort only
+        inv = Inversions(items)
+        self.assertEqual(inv.count(), 1)
+
+        # mixed
+        inv = Inversions(items, 2)
         self.assertEqual(inv.count(), 1)
 
     def test_count_split_inversions(self):
         items = [1, 4, 6, 5, 2]
-        inv = Inversions(items)
-
         # (4,2) (6,5) (6,2) (5,2)
+
+        # merge sort only
+        inv = Inversions(items, 0)
+        self.assertEqual(inv.count(), 4)
+
+        # insertion sort only
+        inv = Inversions(items)
+        self.assertEqual(inv.count(), 4)
+
+        # mixed
+        inv = Inversions(items, 2)
         self.assertEqual(inv.count(), 4)
 
 if __name__ == '__main__':
