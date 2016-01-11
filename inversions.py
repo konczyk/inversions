cbacf61b9da9f76ddd20a11ec726ce3bae595234
Daniel Kończyk <daniel@konczyk.net>
2016-01-11 20:21:53 +0100

    Simplify the merge step

--- inversions.py
+++ inversions.py
@@ -9,9 +9,11 @@ class Inversions:
     def __init__(self, items):
         """
         Copy the list of items and set inversions to 0
+        Create tmp list of the same size as items to hold merge data
         """
 
         self._items = items[:]
+        self._tmp = [0] * len(self._items)
         self._inversions = 0
 
     def count(self):
@@ -48,38 +50,32 @@ class Inversions:
         Merge subarrays indicated by the passed boundaries
         """
 
-        # temp storage for sorted sublist
-        tmp = []
+        # copy subarrays being merged into the tmp list
+        self._tmp[left:right+1] = self._items[left:right+1]
 
-        i, j = left, mid + 1
-
-        # classic sort traversal
-        while i <= mid and j <= right:
-            if self._items[i] <= self._items[j]:
-                tmp.append(self._items[i])
-                i += 1
-            else:
-                tmp.append(self._items[j])
-                # bump inversion count by the amount of items
-                # left in the left sublist
-                self._inversions += (mid - i + 1)
+        i, j, k = left, mid + 1, left
+        # loop until we visit all the values from left to right
+        while k <= right:
+            # left subarray is exausted
+            if i > mid:
+                self._items[k] = self._tmp[j]
                 j += 1
+            # right subarray is exausted
+            elif j > right:
+                self._items[k] = self._tmp[i]
+                i += 1
+            # compare and copy
+            elif self._tmp[j] < self._tmp[i]:
+                self._items[k] = self._tmp[j]
+                j += 1
+                # bump inversions counter by the number of items
+                # still not merged from the left array
+                self._inversions += (mid - i + 1)
+            else:
+                self._items[k] = self._tmp[i]
+                i += 1
 
-        # shift left leftovers to the right,
-        # if right sublist has been exausted
-        if j > right:
-            last_item = mid
-            move_to = right
-            while last_item >= i:
-                self._items[move_to] = self._items[last_item]
-                move_to -= 1
-                last_item -= 1
-
-        # update items with sorted values
-        idx = left
-        for item in tmp:
-            self._items[idx] = item
-            idx += 1
+            k += 1
 
 if __name__ == '__main__':
     items = []
